c409e6277ecb68f72b023667fcc64a3b0bf728f5
Apollo <johndoe@example.com>
2025-06-11 21:52:20 +0300

    create classes directory

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-import RandomQuote from "./src/RandomQuote.js";
+import RandomQuotesApp from "./src/classes/RandomQuotesApp.js";
 
-function displayCurrentQuote(quote) {
-  const { id, text, author } = quote;
-  const quoteElement = document.getElementById("quote");
-  const quoteTextElement = document.getElementById("quote-text");
-  const quoteAuthorElement = document.getElementById("quote-author");
-  quoteElement.dataset.currentQuoteId = id;
-  quoteTextElement.textContent = `"${text}"`;
-  quoteAuthorElement.textContent = author;
-}
-
-const randomQuoteBtn = document.getElementById("random-quote-btn");
-randomQuoteBtn.addEventListener("click", () => {
-  const randomQuote = RandomQuote.getRandomQuote();
-  displayCurrentQuote(randomQuote);
-});
+new RandomQuotesApp();
