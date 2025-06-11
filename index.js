cc4ffac324158699593d18fcd7f7aafa76f497b1
A11 <ivan.petrov@gmail.com>
2025-06-11 17:43:05 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import RandomQuote from './src/RandomQuote.js';
+
+function displaCurrentQuote(quote) {
+  const { id, text, author} = quote;
+  const quoteElement = document.getElementById("quote");
+  const quoteTextElement = document.getElementById("quote-text");
+  const quoteAuthorElement = document.getElementById("quote-author");
+  quoteElement.dataset.currentQuoteId = id;
+  quoteTextElement.textContent = `"${text}"`;
+  quoteAuthorElement.textContent = author;
+}
+
+
+const randomQuoteBtn = document.getElementById('random-quote-btn')
+randomQuoteBtn.addEventListener('click', () => {
+  const randomQuote = RandomQuote.getRandomQuote()
+  displaCurrentQuote(randomQuote)
+});
---
+++ src/RandomQuote.js
@@ -0,0 +1,14 @@
+import quotes from './data/quotes.js'
+import Quote from './Quote.js'
+import { generateRandomInt } from './utils/math.js'
+
+class RandomQuote {
+    static getRandomQuote() {
+        const randomIndex = generateRandomInt(quotes.length)
+        const {id, text, author} = quotes[randomIndex]
+        return new Quote(id, text, author)
+    }
+}
+
+
+export default RandomQuote;
